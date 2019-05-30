c5c0fd1996e71deeca06a656d870ab65ced97ba7
KTheXIII <24420859+KTheXIII@users.noreply.github.com>
2019-05-30 09:36:20 +0200

    Updates

---
+++ precache-manifest.bfbf1fe58a3ed2a99e7342d02c379faa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12ee496c61b6b19a4fb2",
+    "url": "/static/js/main.a312784a.chunk.js"
+  },
+  {
+    "revision": "782431a7a9fd8d41f546",
+    "url": "/static/js/2.8b2938bc.chunk.js"
+  },
+  {
+    "revision": "12ee496c61b6b19a4fb2",
+    "url": "/static/css/main.37593502.chunk.css"
+  },
+  {
+    "revision": "75075285232d56c768a5e8af5be233af",
+    "url": "/index.html"
+  }
+];
